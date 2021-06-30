91cdca31ef141e1bc14c6a7760bbdc956ccbd60b
thijszoontjes <74897795+thijszoontjes@users.noreply.github.com>
2021-06-30 15:34:52 +0200

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	syle.css

--- koop.php
+++ koop.php
@@ -23,17 +23,31 @@
         <a href="hoofdpagina.php#over" class="btn btn3"><b>Over ons</b></a>
     </div>
 </div>
-<div class="overzicht"><h1>Overzicht Product</h1>
-    <label for="fname">Voornaam:</label>
-    <input type="text" id="fname" name="fname">
-    <label for="fname">Voornaam:</label>
-    <input type="text" id="fname" name="fname">
-    <label for="fname">Voornaam:</label>
-    <input type="text" id="fname" name="fname">
-    <label for="fname">Voornaam:</label>
-    <input type="text" id="fname" name="fname">
+<div class="overzicht"><h1>Overzicht Product <br> Betalen via krediet kaart</h1>
+<div class="row1" id="database">
+            <label for="fname"><i class="fa fa-user"></i>Volledige Naam:</label>
+            <input type="text" id="fname" name="firstname" placeholder="Sjonnie Welling">
+            <label for="email"><i class="fa fa-envelope"></i> Email:</label>
+            <input type="text" id="email" name="email" placeholder="sjonnie@voorbeeld.com">
+            <label for="adr"><i class="fa fa-address-card-o"></i> Postcode + Huisnummer:</label>
+            <input type="text" id="adr" name="address" placeholder="1234AB , 69 ">
+            <label for="city"><i class="fa fa-institution"></i> Stad:</label>
+            <input type="text" id="city" name="city" placeholder="Heemskerk ">
 
+    <div class="row2" id="database">
+        <label for="fname"><i class="fa fa-user"></i>Kaarthouder Naam:</label>
+        <input type="text" id="fname" name="firstname" placeholder="Sjonnie Welling">
+        <label for="email"><i class="fa fa-envelope"></i> Krediet kaart nummer:  </label>
+        <input type="text" id="email" name="email" placeholder="1111-22222-3333-4444">
+        <label for="adr"><i class="fa fa-address-card-o"></i>Verval datum kaart:</label>
+        <input type="text" id="adr" name="address" placeholder="September 2021 ">
+        <label for="city"><i class="fa fa-institution"></i> CVV:</label>
+        <input type="text" id="city" name="city" placeholder="123 ">
+
+    </div>
 </div>
+</div>
+
 <span><a href="#"></a></span>
 <br> <br> <br> <br><br> <br><br>><br> <br> <br> <br><br> <br><br> <br> <br> <br> <br> <br><br> <br><br> <br>
 <div class="footer-dark">
@@ -57,3 +71,27 @@
 </body>
 </html>
 <?php
+if(isset($_POST['submit']))
+{
+    $username="root";
+    $password="";
+    $server="127.0.0.1";
+    $database="bootje";
+
+    $con = mysql_connect($server,$username,$password);
+
+    $sql="INSERT INTO customer (FirstName, Surname, EmailAddress)
+VALUES
+('$_POST[firstname]','$_POST[surname]','$_POST[email]')";
+    $a=mysql_query($sql);
+
+    if (!$a)
+    {
+        echo mysql_error();
+    }
+    else
+    {
+        echo "1 record added";
+    }
+    mysql_close($con);
+}
