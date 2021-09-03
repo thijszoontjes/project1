4587ef8e7d2cb465560d01c373d082a5ac2687fc
thijszoontjes <74897795+thijszoontjes@users.noreply.github.com>
2021-09-03 14:34:16 +0200

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	syle.css

--- database.php
+++ database.php
@@ -4,48 +4,6 @@
 //Benodigde velden: alles
 function checkPaypalFields($inputArray){
     $name = $inputArray["name"];
-    $cardHolder = $inputArray["cardHolder"];
-    $email = $inputArray["email"];
-    $cardNumber = $inputArray["cardNumber"];
-    $address = $inputArray["address"];
-    $expiration = $inputArray["expiration"];
-    $city = $inputArray["city"];
-    $cvv = $inputArray["cvv"];
-
-    if (!empty($naam) && !empty($cardHolder) && !empty($email) && !empty($cardNumber) && !empty($address) && !empty($expiration) && !empty($city) && !empty($cvv)){
-        $host = "localhost";
-        $dbusername = "root";
-        $dbpassword = "";
-        $dbname = "bootje";
-
-        //Create connection
-        $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-
-        if (mysqli_connect_error()) {
-            die('Connect Error (' . mysqli_connect_errno() . ') '
-                . mysqli_connect_error());
-        }
-        else {
-            $sql = "INSERT INTO test (naam, email , postcode , stad , kaarthouder , kaartnummer , vervaldatum , cvv)
-                    values ('$name','$email','$address ',' $city ',' $cardHolder ',' $cardNumber ',' $expiration','$cvv')";
-            if ($conn->query($sql)) {
-                echo "RECORD INSERTED";
-            } else {
-                echo "Error: " . $sql . " " . $conn->error;
-            }
-            $conn->close();
-        }
-    }
-    else{
-        echo "MISSING DATA";
-    }
-}
-
-
-//Bestel-knop fields
-//Benodigde velden: name, email, straat + huisnummer, stad + postcode
-function checkAllFields($inputArray){
-    $name = $inputArray["name"];
     $email = $inputArray["email"];
     $address = $inputArray["address"];
     $city = $inputArray["city"];
@@ -64,8 +22,50 @@ function checkAllFields($inputArray){
                 . mysqli_connect_error());
         }
         else {
-            $sql = "INSERT INTO test (naam, email , postcode , stad)
+            $sql = "INSERT INTO test (naam, email , postcode , stad )
                     values ('$name','$email','$address ','$city')";
+            if ($conn->query($sql)) {
+                echo "RECORD INSERTED";
+            } else {
+                echo "Error: " . $sql . " " . $conn->error;
+            }
+            $conn->close();
+        }
+    }
+    else{
+        echo "MISSING DATA";
+    }
+}
+
+
+//Bestel-knop fields
+//Benodigde velden: name, email, straat + huisnummer, stad + postcode
+function checkAllFields($inputArray){
+    $name = $inputArray["name"];
+    $cardHolder = $inputArray["cardHolder"];
+    $email = $inputArray["email"];
+    $cardNumber = $inputArray["cardNumber"];
+    $address = $inputArray["address"];
+    $expiration = $inputArray["expiration"];
+    $city = $inputArray["city"];
+    $cvv = $inputArray["cvv"];
+
+    if (!empty($name) && !empty($cardHolder) && !empty($email) && !empty($cardNumber) && !empty($address) && !empty($expiration) && !empty($city) && !empty($cvv)){
+        $host = "localhost";
+        $dbusername = "root";
+        $dbpassword = "";
+        $dbname = "bootje";
+
+        //Create connection
+        $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+        if (mysqli_connect_error()) {
+            die('Connect Error (' . mysqli_connect_errno() . ') '
+                . mysqli_connect_error());
+        }
+        else {
+            $sql = "INSERT INTO test (naam, email , postcode , stad , kaarthouder , kaartnummer , vervaldatum , cvv)
+                    values ('$name','$email','$address ',' $city ',' $cardHolder ',' $cardNumber ',' $expiration','$cvv')";
             if ($conn->query($sql)) {
                 echo "RECORD INSERTED";
             } else {
