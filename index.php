25a13dee1d0eada1052389701da1bb638d188bf8
PULUTUR <167929@novacollege.nl>
2021-09-10 13:05:48 +0200

    Initial commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <title>Beautjes Cadeatjes</title>
+    <title>Bootjes Cadeatjes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="test.css">
--- product.php
+++ product.php
@@ -5,13 +5,14 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="index.php">
     <link rel="stylesheet" href="jquery ">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title>product</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <style>
     body {
-        background-color: rgba(255,204,204, 0.4);
+        background-color: rgba(255, 204, 204, 0.4);
     }
 </style>
 <body>
@@ -43,14 +44,15 @@
 <!--        </div>-->
 <!---->
 <!--    </div>-->
-<div class="containers py-4 my-4 mx-auto d-flex flex-column">
+<div class="containers py-4 my-4 mx-auto d-flex flex-column" style="height: 800px">
     <div class="header">
-        <div class="row r1">
+        <div class="row r1" style="margin-left: 30%; margin-right: 20%">
             <div class="col-md-9 abc">
                 <h1>Sleutelhanger kat </h1>
                 <h3>Prijs: $2.99</h3>
             </div>
-            <div class="col-md-3 text-right pqr"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
+            <div class="col-md-3 text-right pqr"><i class="fa fa-star"></i><i class="fa fa-star"></i><i
+                        class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></div>
 
         </div>
     </div>
@@ -68,7 +70,7 @@
                     <li>COD Available (All Over India)</li>
                 </ul>
             </div>
-            <div class="col-md-7"> <img id="productkat" src="images/lucky_cat.png" width="70%" height="75%"> </div>
+            <div class="col-md-7"><img id="productkat" src="images/lucky_cat.png" width="70%" height="75%"></div>
 
         </div>
     </div>
